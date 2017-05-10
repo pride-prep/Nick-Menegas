badfe0be2e9ca7271d579d1513ebc40bc33eafd0
Hugh Mungus <IrVegtable@users.noreply.github.com>
2017-05-10 13:15:00 -0700

    HW2 Work
    I worked on HW2 and HW3 some.

---
+++ HW3/HW3/DataViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DataViewController.swift
+//  HW3
+//
+//  Created by Student on 5/10/17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+}
+
+}
