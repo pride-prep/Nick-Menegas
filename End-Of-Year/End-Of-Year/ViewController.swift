41be562fc86d397a7ac9068ba1c67bd883544463
Hugh Mungus <IrVegtable@users.noreply.github.com>
2017-06-06 12:26:01 -0700

    Layout is Done
    We have the main layout and we still have some effects to make

---
+++ End-Of-Year/End-Of-Year/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  End-Of-Year
+//
+//  Created by Student on 5/30/17.
+//  Copyright © 2017 Anything. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func proceedButton(_ sender: Any) {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
+
